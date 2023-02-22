14a4cf050195d79645360833a2c12a973a4770a2
DevExpressExampleBot <fakeEmail>
2023-02-22 19:37:23 +0400

    Source auto update [skip ci]

---
+++ VB/DisplayGroupSummaries_CodeBehind/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace DisplayGroupSummaries_CodeBehind
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/DisplayGroupSummaries_MVVM/MainViewModel.vb
+++ VB/DisplayGroupSummaries_MVVM/MainViewModel.vb
@@ -1,61 +1,43 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.DataAnnotations
 Imports DevExpress.Mvvm.Xpf
 Imports System
 Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
-Imports System.Linq
 
 Namespace DisplayGroupSummaries_MVVM
-	Public Class Account
-		Public Property UserName() As String
-		Public Property RegistrationDate() As DateTime
-		Public Property Married() As Boolean
-		Public Property Age() As Integer
-	End Class
-	Public Class MainViewModel
-		Inherits ViewModelBase
 
-		Public ReadOnly Property AccountList() As ObservableCollection(Of Account)
-		Public Sub New()
-			AccountList = New ObservableCollection(Of Account)(GetAccountList())
-		End Sub
-		Private Iterator Function GetAccountList() As IEnumerable(Of Account)
-			Yield New Account() With {
-				.UserName = "Nick White",
-				.RegistrationDate = DateTime.Today,
-				.Married = True,
-				.Age = 42
-			}
-			Yield New Account() With {
-				.UserName = "Jack Rodman",
-				.RegistrationDate = New DateTime(2009, 5, 10),
-				.Married = False,
-				.Age = 30
-			}
-			Yield New Account() With {
-				.UserName = "Sandra Sherry",
-				.RegistrationDate = New DateTime(2008, 12, 22),
-				.Married = False,
-				.Age = 18
-			}
-			Yield New Account() With {
-				.UserName = "Sabrina Vilk",
-				.RegistrationDate = DateTime.Today,
-				.Married = True,
-				.Age = 24
-			}
-			Yield New Account() With {
-				.UserName = "Mike Pearson",
-				.RegistrationDate = New DateTime(2008, 10, 18),
-				.Married = True,
-				.Age = 37
-			}
-		End Function
+    Public Class Account
 
-		<Command>
-		Public Sub CustomSummaryExistsCommand(ByVal args As RowSummaryExistsArgs)
-			args.Exists = args.GroupPath(0).GroupLevel = 0
-		End Sub
-	End Class
+        Public Property UserName As String
+
+        Public Property RegistrationDate As Date
+
+        Public Property Married As Boolean
+
+        Public Property Age As Integer
+    End Class
+
+    Public Class MainViewModel
+        Inherits ViewModelBase
+
+        Public ReadOnly Property AccountList As ObservableCollection(Of Account)
+
+        Public Sub New()
+            AccountList = New ObservableCollection(Of Account)(GetAccountList())
+        End Sub
+
+        Private Iterator Function GetAccountList() As IEnumerable(Of Account)
+            Yield New Account() With {.UserName = "Nick White", .RegistrationDate = Date.Today, .Married = True, .Age = 42}
+            Yield New Account() With {.UserName = "Jack Rodman", .RegistrationDate = New DateTime(2009, 5, 10), .Married = False, .Age = 30}
+            Yield New Account() With {.UserName = "Sandra Sherry", .RegistrationDate = New DateTime(2008, 12, 22), .Married = False, .Age = 18}
+            Yield New Account() With {.UserName = "Sabrina Vilk", .RegistrationDate = Date.Today, .Married = True, .Age = 24}
+            Yield New Account() With {.UserName = "Mike Pearson", .RegistrationDate = New DateTime(2008, 10, 18), .Married = True, .Age = 37}
+        End Function
+
+        <Command>
+        Public Sub CustomSummaryExistsCommand(ByVal args As RowSummaryExistsArgs)
+            args.Exists = args.GroupPath(0).GroupLevel = 0
+        End Sub
+    End Class
 End Namespace
